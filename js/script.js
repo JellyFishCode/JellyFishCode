740cc23b4f7feb32741e71e42cf019ba217380f6
JellyFishCode <73017822+JellyFishCode@users.noreply.github.com>
2022-03-31 13:37:40 +0200

    portfolio2022
    Changed projects & structure

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-const navToggle = document.querySelector('.nav-toggle'); // sucht die Klasse aus dem html aus
+const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const images = document.querySelectorAll('.open');
+const close = document.getElementById("close");
+const modal = document.getElementById("modal");
+var modal__container = document.getElementById("modal__container");
 
+// ----------------- Popup Image -----------------
+images.forEach(img => {
+  img.addEventListener('click', (e) => {
+    modal.src = e.target.alt;
+    modal__container.classList.add('show');
+  })
+})
 
+close.addEventListener('click', () => {
+  console.log("close");
+  modal__container.classList.remove('show');
+});
+
+// ----------------- Navigation -----------------
 navToggle.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
-}); // öffnet die Navigationsleiste (Wenn man auf die Klasse nav-open klickt, wird dieser ausfgeführt, hier Navigationsleiste)
+});
 
 navLinks.forEach(link => {
 	link.addEventListener('click', () => {
@@ -12,7 +29,7 @@ navLinks.forEach(link => {
 	})
 })
 
-// Slider -----------------
+// ----------------- Slider -----------------
 var slideIndex = 1;
 showSlides(slideIndex);
 
