6ef227d32f8c37e6f022fef1e3f46960eb898dde
JellyFishCode <73017822+JellyFishCode@users.noreply.github.com>
2022-04-01 21:30:44 +0200

    navigation bugfix

--- js/script.js
+++ js/script.js
@@ -5,26 +5,28 @@ const close = document.getElementById("close");
 const modal = document.getElementById("modal");
 var modal__container = document.getElementById("modal__container");
 
+// ----------------- Navigation -----------------
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
+});
+
+
 // ----------------- Popup Image -----------------
 images.forEach(img => {
   img.addEventListener('click', (e) => {
     modal.src = e.target.alt;
     modal__container.classList.add('show');
-  })
-})
+  });
+});
 
 close.addEventListener('click', () => {
   console.log("close");
   modal__container.classList.remove('show');
 });
 
-// ----------------- Navigation -----------------
-navToggle.addEventListener('click', () => {
-	document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-	link.addEventListener('click', () => {
-		document.body.classList.remove('nav-open');
-	})
-})
